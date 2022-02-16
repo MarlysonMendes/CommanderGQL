598d163d820fb2b87900bc4f2e592a60d44bfb32
Marlyson Mendes <marlysonmendes@gmail.com>
2022-02-16 17:33:31 -0300

    Adicionando os endpoint
    esses  dois endpoint vão ajudar a depurar nossa aplicação um fornecendo uma interface para testar os post e o outro fornecendo o esquema das query

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using CommanderGQL.Data;
 using CommanderGQL.GraphQL;
 using Microsoft.EntityFrameworkCore;
+using GraphQL.Server.Ui.Voyager;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,11 +12,14 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddGraphQLServer().AddQueryType<Query>();
 
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
 app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapGraphQL();
+    });
+app.UseGraphQLVoyager(new VoyagerOptions()
 {
-    endpoints.MapGraphQL();
-});
+    GraphQLEndPoint = "/graphql"
+}, "/graphiql-voyager");
 
 app.Run();
